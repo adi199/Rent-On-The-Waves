a76d76582b7035a501718c9be4e76e29bfb91c77
karumuru Siddhartha <siddharthakv@clickwt.com>
2021-11-29 00:44:20 -0600

    sid-branch,added mahesh files

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/signup', function(req, res, next) {
+  res.render('signUpForm', { title: 'signup' });
+});
+
+/* GET home page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'login' });
+});
+
+
+
 module.exports = router;
